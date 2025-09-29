11710dd4daf5ec0bd527ccac67e2f49e2fef2beb
Lucasbomgon <lucas.bom.gon2132@gmail.com>
2025-09-29 09:38:13 -0300

    datacontext

--- back/src/ProEventos.API/Controllers/EventoController.cs
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -10,11 +10,11 @@ namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class EventoController : ControllerBase // herda de ControllerBase
     {
 
 
-        public IEnumerable<Evento> _evento = new Evento[]
+        public IEnumerable<Evento> _evento = new Evento[] // foi criado um evento erdado de evento
         {
             new Evento() {
                     EventoId = 1,
@@ -25,7 +25,7 @@ namespace ProEventos.API.Controllers
                     DataEvento = DateTime.Now.AddDays(2).ToString(),
                     ImagemURL = "foto.png"
                 },
-            new Evento() {
+            new Evento() { 
                     EventoId = 2,
                     Tema = "Angular e Suas novidades",
                     Local = "Curitiba",
@@ -43,9 +43,9 @@ namespace ProEventos.API.Controllers
 
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public IEnumerable<Evento> Get(int id) // metodo get que recebe um id
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _evento.Where(evento => evento.EventoId == id); // retorna o evento que tem o id igual ao id passado
 
         }
 
